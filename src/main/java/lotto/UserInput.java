9addfbd87f5fa72a127e67ee2104aa3d78b5e12d
JiEung2 <https://github.com/JiEung2>
2023-07-18 10:43:25 +0900

    기본적인 입출력 기능 구현 기본적인 기능만 구현해놨기 때문에 메소드, 클래스 등 정리 필요함 많은 수정 필요함,, ㅎㅎ

---
+++ src/main/java/lotto/UserInput.java
@@ -0,0 +1,60 @@
+package lotto;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class UserInput {
+
+    User user;
+    List<Integer> numbers;
+
+    public UserInput(User user){
+        this.user = user;
+    }
+
+    //유저의 구입금액을 입력받는 메소드
+    public void inputPrice(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String tmp = readLine();
+        int price = Integer.parseInt(tmp);
+        user.buyLotto(price);
+        user.setPrice(price);
+    }
+
+    //당첨 번호를 입력받는 메소드
+    public void inputNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] tmpNumbers = readLine().split(",");
+        for(int i=0; i< tmpNumbers.length; i++) {
+            this.numbers.add(Integer.parseInt(tmpNumbers[i]));
+        }
+        validate(numbers);
+    }
+
+    //보너스 번호를 입력받는 메소드
+    public void inputBonusNum(){
+        System.out.println("보너스 번호를 입력해주세요");
+        int bonusNum = Integer.parseInt(readLine());
+        validate(bonusNum);
+        user.setBonusNum(bonusNum);
+    }
+
+    // 번호가 1에서 45사이인지 유효성 검사
+    // 에러 시 "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." 라는 문구 출력되게 수정
+    private void validate(int numbers){
+            if(numbers<1 || numbers>45){
+                throw new IllegalArgumentException();
+            }
+    }
+
+    private void validate(List<Integer> numbers){
+        for(int i=0; i<numbers.size(); i++){
+            if(numbers.get(i)<1 || numbers.get(i)>45){
+                throw new IllegalArgumentException();
+            }
+        }
+
+    }
+
+}
